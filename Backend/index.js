bbaf224337ccba01ac2aa104fafcea52a7142441
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-10-05 03:45:22 +0530

    new commit

--- Backend/index.js
+++ Backend/index.js
@@ -21,11 +21,6 @@ async function main(){
     await mongoose.connect(DB_URL);
     
 }
-Middleware
-app.use(cors({
-    origin: ['https://scettnp-frontend.onrender.com','http://192.168.21.3:3000/'], // Specify your frontend origin
-    credentials: true // Allow cookies to be sent with requests
-}));
 
 main().then(console.log('Database Connected')).catch((err)=>{console.log(`error in connecting database ${err}`)})
 
