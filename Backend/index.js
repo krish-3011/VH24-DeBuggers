3803ea1d6d6d715f355a2ad65a39ae69ae22f159
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-10-05 03:59:06 +0530

    new commit

--- Backend/index.js
+++ Backend/index.js
@@ -9,29 +9,53 @@ const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
+const cors = require('cors');
 require('dotenv').config();
 
-const PORT = process.env.PORT
-const DB_URL = process.env.DB_URL
-const SECURE_CODE = process.env.SECURE_CODE
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+const SECURE_CODE = process.env.SECURE_CODE;
 
-//connectin to database
-async function main(){
+// Connecting to database
+async function main() {
     await mongoose.connect(DB_URL);
-    
 }
 
-main().then(console.log('Database Connected')).catch((err)=>{console.log(`error in connecting database ${err}`)})
+// Whitelist of allowed origins
+const whitelist = ['https://vh24-debuggers-frontend.onrender.com'];
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+// Set up the CORS options
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);  // Allow the request if it's in the whitelist
+        } else {
+            callback(new Error('Not allowed by CORS'));  // Block the request if it's not in the whitelist
+        }
+    },
+    credentials: true,  // Allow credentials like cookies to be sent in requests
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed headers
+};
+
+// ** Use CORS with the specified options **
+app.use(cors(corsOptions));  // This is where the CORS configuration is applied
+
+main()
+    .then(() => console.log('Database Connected'))
+    .catch((err) => {
+        console.log(`Error in connecting database: ${err}`);
+    });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(flash());
 
-//set up session
+// Set up session
 app.use(session({
     secret: SECURE_CODE, 
     resave: false,
@@ -46,10 +70,11 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-//routes
-app.use('/deliveryPartner',deliveryPartner)
-app.use('/restaurant',restaurant)
+// Routes
+app.use('/deliveryPartner', deliveryPartner);
+app.use('/restaurant', restaurant);
 
-app.listen(PORT,()=>{
-    console.log(`Server is listng on port ${PORT}`);
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
