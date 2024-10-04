0e09f9b9d7483352a68c314525446a61b6e6c411
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-10-05 04:55:43 +0530

    eight-proto

--- vh_frontend/src/profile/profile.jsx
+++ vh_frontend/src/profile/profile.jsx
@@ -41,6 +41,7 @@ const Profile = () => {
           <Header1 /> {/* Header is always rendered */}
         </div>
         <div className="profile-content">
+          {/* If not logged in, show the login page, otherwise show the profile */}
           {showLoginPage ? (
             <LoginComponent onSubmit={handleLoginSubmit} />
           ) : profileData ? (
@@ -61,6 +62,17 @@ const Profile = () => {
 
               <h3>Rank: </h3>
               <p>{profileData.rank}</p>
+
+              {/* Render the two cards */}
+              <div className="two-cards">
+                <div className="card1">
+                  <h3>Region:</h3>
+                  <p>{profileData.region}</p> {/* Use dynamic data from profileData */}
+                </div>
+                <div className="card2">
+                  <p>hello</p>
+                </div>
+              </div>
             </div>
           ) : (
             <div>Loading profile data...</div>
