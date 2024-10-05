03023f92deb58a58fb73346a003a9de3075ae72f
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-10-05 12:08:25 +0530

    ten-proto

--- vh_frontend/src/dashboard/First.jsx
+++ vh_frontend/src/dashboard/First.jsx
@@ -3,52 +3,52 @@ import './Dashboard.css';
 import NavBar from "./NavBar";
 import Header1 from "./Header";
 import DashboardGrid from "./DashboardGrid";
-import LoginComponent from "../login/login";
-import Cookies from 'js-cookie';
+// import LoginComponent from "../login/login";
+// import Cookies from 'js-cookie';
 
 const FirstPage = () => {
-  const [showLoginPage, setShowLoginPage] = useState(true);
-  const [profileData, setProfileData] = useState(null);
+//   const [showLoginPage, setShowLoginPage] = useState(true);
+//   const [profileData, setProfileData] = useState(null);
 
-  const handleLoginSubmit = (success) => {
-    if (success) {
-      const userCookie = Cookies.get('user'); // Read session cookie
-      if (userCookie) {
-        const data = JSON.parse(userCookie);
-        setProfileData(data);
-        setShowLoginPage(false);
-      }
-    } else {
-      setShowLoginPage(true);
-    }
-  };
+//   const handleLoginSubmit = (success) => {
+//     if (success) {
+//       const userCookie = Cookies.get('user'); // Read session cookie
+//       if (userCookie) {
+//         const data = JSON.parse(userCookie);
+//         setProfileData(data);
+//         setShowLoginPage(false);
+//       }
+//     } else {
+//       setShowLoginPage(true);
+//     }
+//   };
 
-  useEffect(() => {
-    const userCookie = Cookies.get('user'); // Read session cookie
-    if (userCookie) {
-      const data = JSON.parse(userCookie);
-      setProfileData(data);
-      setShowLoginPage(false); // Hide login page if user data is found
-    } else {
-      setShowLoginPage(true); // Show login page if no user data is found
-    }
-  }, []);
+//   useEffect(() => {
+//     const userCookie = Cookies.get('user'); // Read session cookie
+//     if (userCookie) {
+//       const data = JSON.parse(userCookie);
+//       setProfileData(data);
+//       setShowLoginPage(false); // Hide login page if user data is found
+//     } else {
+//       setShowLoginPage(true); // Show login page if no user data is found
+//     }
+//   }, []);
 
   return (
     <div className="container">
       <NavBar />
       <div className="innercont">
         <Header1 />
-         {showLoginPage ? (
+         {/* {showLoginPage ? (
           <LoginComponent onSubmit={handleLoginSubmit} />
         ) : (
-          profileData ? (
+          profileData ? (*/}
             <DashboardGrid />
-            
+            {/*}
           ) : (
             <div>Loading profile data...</div>
           )
-        )} 
+        )}  */}
       </div>
     </div>
   );
